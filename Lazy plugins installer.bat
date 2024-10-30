13cbdbb32114e8be59ccdbd34052bf5122c3e4f2
Bat23may <95421018+Bat23may@users.noreply.github.com>
2024-10-30 21:40:25 +1100

    Add files via upload

---
+++ Lazy plugins installer.bat
@@ -0,0 +1,55 @@
+@echo off
+echo Cloning GitHub Repositories 
+echo this might take some time
+::this is all the link from https://endless-sky.github.io/plugins.html
+::put :: to disable the ones you dont wont
+git clone https://github.com/mathwhiz1212/A-Coalition-At-War
+git clone https://github.com/mathwhiz1212/ES-conscience-plugin
+git clone https://github.com/endless-sky/all-content-plugin
+git clone https://github.com/Saugia/arc-over
+git clone https://github.com/Spectre907YT/Arion-Air_Superiority
+git clone https://github.com/beccabunny/Beccas-Endless-Expansion
+git clone https://github.com/ZBok/BetterVanillaPirates/
+git clone https://github.com/ZBok/BetterVanillaHai
+git clone https://github.com/mOctave/blended-ships/
+git clone https://github.com/demolish238/Capitalis-Major
+git clone https://github.com/Saugia/celestial-strands
+git clone https://github.com/RestingImmortal/Ciu-Kreo
+git clone https://github.com/DJF113/Core-Mining-Inc
+git clone https://github.com/leklachu/endless-sky-syndicate
+git clone https://github.com/OcelotWalrus/Cromha-Expansion-plugin
+git clone https://github.com/TheGiraffe3/es-cutlass/
+git clone https://github.com/Daeridanii1/DAIS
+git clone https://github.com/kestrel1110/Easier-Korath-Automatons
+git clone https://github.com/Autohummer/Endless-Sky-Starbridge-Pack
+git clone https://github.com/kestrel1110/ES-Advanced-Starts
+git clone https://github.com/Cromha-Plugins/es-ui-upgrades
+git clone https://github.com/comnom/Eternals
+git clone https://github.com/kestrel1110/Exporianes
+git clone https://github.com/1010todd/Fighter-Factory
+git clone https://github.com/Cromha-Plugins/Fire-Corporation
+git clone https://github.com/MIMSBZ3/Gaguka
+git clone https://github.com/1010todd/Galactic-War
+git clone https://github.com/endless-sky/endless-sky-high-dpi
+git clone https://github.com/endless-sky/endless-sky-high-dpi
+git clone https://github.com/TheGiraffe3/Jibauni
+git clone https://github.com/kestrel1110/KineticWeaponry
+git clone https://github.com/demolish238/Korath-Retribution
+git clone https://github.com/Zitchas/ES_Lampyrid_Start
+git clone https://github.com/MidnightPlugins/Logbook-Help
+git clone https://github.com/MidnightPlugins/Lost-in-Midnight
+git clone https://github.com/ziproot/endless-sky-main-plot-plus
+git clone https://github.com/1010todd/Mega-Freight
+git clone https://github.com/MidnightPlugins/Midnight-Expansion
+git clone https://github.com/MidnightPlugins/Midnight-Scrapyard
+git clone https://github.com/demolish238/Mil-taj-Protectorate.git
+git clone https://github.com/fracturedprism/Old-Bounty-Hunting-Missions
+git clone https://github.com/EndlessSkyCommunity/-Omnis
+git clone https://github.com/MidnightPlugins/Outfit-Highlighter
+git clone https://github.com/Anarchist2/ES-news-portraits
+git clone https://github.com/Startingfresher/Prism-Plug-in
+git clone https://github.com/demolish238/QOL-Outfits
+git clone https://github.com/AvianGeneticist/QuaernanHardpointsCarries
+
+echo Done! Enjoy your mods :3
+pause
